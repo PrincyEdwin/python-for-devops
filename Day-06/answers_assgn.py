bb84f3defaa9aa592a223c1c92ffefb0e3ea0fb3
Mahiba Princy <89736159+PrincyEdwin@users.noreply.github.com>
2025-04-25 22:49:43 +0000

    Add assignment file

---
+++ Day-06/answers_assgn.py
@@ -0,0 +1,30 @@
+# Addition
+
+a = 5
+b = 4
+print("Addition", a + b)
+
+# Subtraction
+
+print("Subtraction", a-b)
+
+# Multiplication
+
+print("Multiplication", a*b)
+
+
+# Floor Division
+
+print("Floor Division", a//b)
+
+# Division
+
+print("Division", a/b)
+
+# Modulus
+
+print("Modulus", a%b)
+
+# Exponents
+
+print("Exponents", a**b)
